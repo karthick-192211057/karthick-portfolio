d29e6835f34bf5207cc18a9cd5d2c9f21b5e9675
Karthick S <karthicks1057.sse@saveetha.com>
2026-04-08 17:08:10 +0530

    Fix static files for Vercel

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 // ======================
 app.set("view engine", "ejs");
-
+app.set("views", path.join(__dirname, "views"));
 // ✅ Static files (IMPORTANT for Vercel)
 app.use(express.static(path.join(__dirname, "public")));
 
